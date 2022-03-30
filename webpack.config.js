c3e03d07cab4ee7d7abfc7e860e0a78d40154c30
Mark Alperin <mark.t.alperin@gmail.com>
2022-03-30 17:10:03 -0500

    removed extraneous comments

--- webpack.config.js
+++ webpack.config.js
@@ -15,13 +15,6 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react']
-            // plugins: [
-            //   ["@babel/plugin-transform-runtime",
-            //     {
-            //       "regenerator": true
-            //     }
-            //   ]
-            // ]
           }
         }
       },
