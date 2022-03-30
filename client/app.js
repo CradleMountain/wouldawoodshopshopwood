299b6d97beeb333078c2298aaf3c4caf9d7e6aca
Elfric <elf72742@gmail.com>
2022-03-30 13:54:38 -0400

    Add react-dom library; implements basic structure of App component

--- client/app.js
+++ client/app.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import ProductOverview from './widgets/product-overview/productOverview.js';
+import QuestionsAnswers from './widgets/questions-answers/questionsAnswers.js';
+import RatingsReviews from './widgets/ratings-reviews/ratingsReviews.js';
+import RelatedProducts from './widgets/related-products/relatedProducts.js';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <ProductOverview />
+        <RelatedProducts />
+        <QuestionsAnswers />
+        <RatingsReviews />
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
