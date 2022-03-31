01ea84b959bf665083f2df162d17bb6564d973e5
Elfric <elf72742@gmail.com>
2022-03-31 15:38:42 -0400

    App sends get request for single product by product_id

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import ProductOverview from './widgets/product-overview/productOverview.js';
 import QuestionsAnswers from './widgets/questions-answers/questionsAnswers.js';
@@ -8,6 +9,32 @@ import RelatedProducts from './widgets/related-products/relatedProducts.js';
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      products: [],
+      currentProduct: {}
+    }
+
+    this.getProductById = this.getProductById.bind(this);
+  }
+
+  componentDidMount() {
+    this.getProductById(37312);
+  }
+
+  getProductById(id) {
+    axios({
+      method: 'GET',
+      url: `/products/${id}`
+    })
+      .then(({data}) => {
+        this.setState({
+          currentProduct: data
+        })
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   render() {
