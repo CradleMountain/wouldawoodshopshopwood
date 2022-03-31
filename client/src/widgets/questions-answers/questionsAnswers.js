57dca9416c42e1d0da3a2b037585c1bd8e441363
Cory Finkbeiner <portland.steeper@gmail.com>
2022-03-31 15:12:13 -0400

    3/31 updating main at 3:12 pm

--- client/src/widgets/questions-answers/questionsAnswers.js
+++ client/src/widgets/questions-answers/questionsAnswers.js
@@ -1,1 +1,16 @@
 
+class QuestionsAnswers extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        QuestionsAnswers
+      </>
+    );
+  }
+}
+
+export default App;
