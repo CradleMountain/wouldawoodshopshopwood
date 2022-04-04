37b6950b3b7ef6e5e8ebd742486ad8113119a3df
Elfric <elf72742@gmail.com>
2022-04-04 14:42:09 -0400

    Ratings-reviews widget set up to render reviewlist

--- client/src/widgets/ratings-reviews/ratingsReviews.js
+++ client/src/widgets/ratings-reviews/ratingsReviews.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import RatingBreakdown from './components/ratingBreakdown.js';
 import ProductBreakdown from './components/productBreakdown.js';
+import ReviewList from './components/reviewList.js'
 
 const RatingsReviews = (props) => {
   const getMetadata = (productId) => {
@@ -21,7 +22,28 @@ const RatingsReviews = (props) => {
       });
   }
 
+  const getReviews = (productId) => {
+    return axios({
+      method: 'GET',
+      url: '/reviews',
+      params: {
+        'product_id': productId,
+        'page': 1,
+        'count': 2,
+        'sort': 'relevant'
+      }
+    })
+      .then(({data}) => {
+        return data;
+      })
+      .catch((err) => {
+        return err;
+      })
+  };
+
+  const [product, setProduct] = useState(props.product.id);
   const [metadata, setMetadata] = useState({});
+  const [reviews, setReviews] = useState({});
 
   if (props.product.id && Number(metadata.product_id) !== props.product.id) {
     getMetadata(props.product.id)
@@ -33,16 +55,29 @@ const RatingsReviews = (props) => {
       });
   }
 
-  if (metadata.product_id) {
+  if (props.product.id && product !== props.product.id) {
+    getReviews(props.product.id)
+      .then((data) => {
+        setReviews(data.results);
+        setProduct(Number(data.product));
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  if (product) {
     return (
+      <>
+      <h2>Ratings &amp; Reviews</h2>
       <div className="ratings-reviews">
-        <h2>Ratings &amp; Reviews</h2>
         <div className="rr-breakdowns">
           <RatingBreakdown metadata={metadata} product={props.product} />
           <ProductBreakdown factors={metadata.characteristics} />
         </div>
-        <div className="rr-reviews"></div>
+        <ReviewList reviews={reviews}/>
       </div>
+      </>
     );
   } else {
     return (<div className="ratings-reviews"></div>)
