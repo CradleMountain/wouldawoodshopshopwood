2f707e73cbad66f39b2b8a60e0cb729a384349f4
MarkAlperin <mark.t.alperin@gmail.com>
2022-04-12 16:30:20 -0500

    removed un-used import statement

--- client/src/widgets/related-products/components/productCard.js
+++ client/src/widgets/related-products/components/productCard.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
-import _ from "underscore";
 
 import StarRating from "../../../components/starRating.js";
 
