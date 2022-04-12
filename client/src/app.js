6d0664cb776dbd7de1c26c6b89a850cedf241339
Cory Finkbeiner <portland.steeper@gmail.com>
2022-04-12 16:59:04 -0400

    PULL to DEV 2

--- client/src/app.js
+++ client/src/app.js
@@ -1,61 +1,122 @@
-import React from 'react';
-// import ReactDOM from 'react-dom';
-import axios from 'axios';
+import React, { useState, useEffect, Fragment } from "react";
+import axios from "axios";
 
-import ProductOverview from './widgets/product-overview/productOverview.js';
-import QuestionsAnswers from './widgets/questions-answers/questionsAnswers.js';
-import RatingsReviews from './widgets/ratings-reviews/ratingsReviews.js';
-import RelatedProducts from './widgets/related-products/relatedProducts.js';
+import ContextWrapper from "./widgets/product-overview/context/ContextWrapper.js";
+import QuestionsAnswers from "./widgets/questions-answers/questionsAnswers.js";
+import RatingsReviews from "./widgets/ratings-reviews/ratingsReviews.js";
+import RelatedProducts from "./widgets/related-products/relatedProducts.js";
+import NavBar from "./widgets/navigation-bar/navBar.js";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [currentProduct, setCurrentProduct] = useState({});
+  const [products, setProducts] = useState([]);
 
-    this.state = {
-      products: [],
-      currentProduct: {}
-    }
+  useEffect(() => {
+    getProductById(37311);
+  }, []);
 
-    this.getProductById = this.getProductById.bind(this);
-  }
-
-  componentDidMount() {
-    this.getProductById(37313);
-  }
-
-  getProductById(id) {
+  const getProductById = (id) => {
     axios({
-      method: 'GET',
-      url: `/products/${id}`
+      method: "GET",
+      url: `/products/${id}`,
     })
-      .then(({data}) => {
-        this.setState({
-          currentProduct: data
-        })
+      .then((data) => {
+        console.log(data.data);
+        setCurrentProduct(data.data);
       })
       .catch((err) => {
         console.error(err);
       });
-  }
+  };
 
-  render() {
-    return (
-
-        <QuestionsAnswers currentProduct={this.state.currentProduct} />
-
-    );
-  }
-
-  // render() {
-  //   return (
-  //     <>
-  //       <ProductOverview />
-  //       <RelatedProducts />
-  //       <QuestionsAnswers />
-  //       <RatingsReviews />
-  //     </>
-  //   );
-  // }
-}
+  return (
+    <Fragment>
+      <NavBar />
+      <div className="widgets-main">
+        {Object.keys(currentProduct).length !== 0 && (
+          <ContextWrapper currentProduct={currentProduct} />
+        )}
+        <RelatedProducts product={currentProduct} getProductById={getProductById}/>
+        <QuestionsAnswers currentProduct={currentProduct} />
+        <RatingsReviews product={currentProduct} />
+      </div>
+    </Fragment>
+  );
+};
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// // import ReactDOM from 'react-dom';
+// import axios from 'axios';
+
+// import ProductOverview from './widgets/product-overview/productOverview.js';
+// import QuestionsAnswers from './widgets/questions-answers/questionsAnswers.js';
+// import RatingsReviews from './widgets/ratings-reviews/ratingsReviews.js';
+// import RelatedProducts from './widgets/related-products/relatedProducts.js';
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       products: [],
+//       currentProduct: {}
+//     }
+
+//     this.getProductById = this.getProductById.bind(this);
+//   }
+
+//   componentDidMount() {
+//     this.getProductById(37313);
+//   }
+
+//   getProductById(id) {
+//     axios({
+//       method: 'GET',
+//       url: `/products/${id}`
+//     })
+//       .then(({data}) => {
+//         this.setState({
+//           currentProduct: data
+//         })
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//       });
+//   }
+
+//   render() {
+//     return (
+
+//         <QuestionsAnswers currentProduct={this.state.currentProduct} />
+
+//     );
+//   }
+
+//   // render() {
+//   //   return (
+//   //     <>
+//   //       <ProductOverview />
+//   //       <RelatedProducts />
+//   //       <QuestionsAnswers />
+//   //       <RatingsReviews />
+//   //     </>
+//   //   );
+//   // }
+// }
+
+// export default App;
