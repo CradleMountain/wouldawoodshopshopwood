6d96e5e1a7202fa759c3e778667ec586f6728471
MarkAlperin <mark.t.alperin@gmail.com>
2022-04-13 13:43:18 -0500

    allowing all origins in cors header

--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ const { GITHUB_API_KEY } = require('../config.js');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
