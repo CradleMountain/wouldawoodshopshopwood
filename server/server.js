5a9091c3089fd9c035bdf29c40750dc7b15b3d36
MarkAlperin <mark.t.alperin@gmail.com>
2022-04-13 14:04:46 -0500

    added request types to cors header

--- server/server.js
+++ server/server.js
@@ -1,43 +1,45 @@
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
-const { GITHUB_API_KEY } = require('../config.js');
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const { GITHUB_API_KEY } = require("../config.js");
 
 const app = express();
 
-app.use(cors({
-  origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/upload', express.static(path.join(__dirname, '../client/dist/upload.html')));
+app.use(
+  "/upload",
+  express.static(path.join(__dirname, "../client/dist/upload.html"))
+);
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.use('/*', (req, res) => {
+app.use("/*", (req, res) => {
   req.url = req.baseUrl;
-  if (!(['/', '/favicon.ico', '/upload']).includes(req.url)) {
+  if (!["/", "/favicon.ico", "/upload"].includes(req.url)) {
     axios({
       method: req.method,
       url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe${req.url}`,
       data: req.body,
       headers: {
-        'Authorization': GITHUB_API_KEY
+        Authorization: GITHUB_API_KEY,
       },
-      params: req.query
+      params: req.query,
     })
       .then((response) => {
-        res.set(response.headers)
-          .status(response.status)
-          .send(response.data);
+        res.set(response.headers).status(response.status).send(response.data);
       })
       .catch((err) => {
         if (err.response) {
-          res.set(err.response.headers)
-          .status(err.response.status)
-          .send();
+          res.set(err.response.headers).status(err.response.status).send();
         } else {
           res.status(500).send();
         }
@@ -47,4 +49,6 @@ app.use('/*', (req, res) => {
   }
 });
 
-app.listen(3000, () => { console.log('Listening on port 3000...'); });
+app.listen(3000, () => {
+  console.log("Listening on port 3000...");
+});
