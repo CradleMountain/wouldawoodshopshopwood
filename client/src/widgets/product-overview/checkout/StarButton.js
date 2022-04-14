beb5aebcd8e11f0390d38df4e1c454e3180526b1
MarkAlperin <mark.t.alperin@gmail.com>
2022-04-14 14:58:12 -0500

    removed console.log from starButton

--- client/src/widgets/product-overview/checkout/StarButton.js
+++ client/src/widgets/product-overview/checkout/StarButton.js
@@ -10,19 +10,12 @@ const StarButton = (props) => {
 
   const clickHandler = () => {
     setIsStar(!isStar);
-    api.getCart()
-      .then(data => {
-        console.log('Cart Data: ', {data})
-      })
   };
 
   return (
     <div className="po-drop-down-container po-star" onClick={clickHandler}>
       {isStar && <div><i className="fa-solid fa-star fa-lg"></i></div>}
       {!isStar && <div><i className="fa-regular fa-star fa-lg"></i></div>}
-      {/* <div className="dropDownButton" onClick={clickHandler}>
-        <p>/p>
-      </div> */}
     </div>
   );
 };
