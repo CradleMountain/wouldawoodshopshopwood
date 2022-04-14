f9fc08a862c338b8d680ea0b66716e71664abde3
Elfric <elf72742@gmail.com>
2022-04-14 17:17:32 -0400

    Alt text for images in reviews widget

--- client/src/widgets/ratings-reviews/components/imageLoader.js
+++ client/src/widgets/ratings-reviews/components/imageLoader.js
@@ -42,7 +42,7 @@ const ImageLoader = ({state, setState, validate}) => {
       <div className="rr-up-thumbs">
         {photos.map((url, i) => {
           return (<div key={i} className="rr-up-thumb">
-            <img src={url} onError={() => invalidUrl(i + 1)}/>
+            <img src={url} onError={() => invalidUrl(i + 1)} alt="Uploaded image thumbnail"/>
             <div onClick={() => {removePhoto(i);}}>
               <span className="rr-up-remove fa-layers fa-fw">
                 <FontAwesomeIcon icon="fa-solid fa-circle" className="icon-white"/>
